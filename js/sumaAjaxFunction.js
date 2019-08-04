d2e64a4352e3e2f833125b07f9cb633bd985a004
manur27 <manuelangelini2@gmail.com>
2019-08-04 16:47:10 +0200

    Condicionales adicionales y ajuste de función para restringir caracteres en inputs

--- js/sumaAjaxFunction.js
+++ js/sumaAjaxFunction.js
@@ -19,9 +19,43 @@ function sumaAJAX(num1, num2){
         }
     });
 }
+    
+    function validateFloatKeyPress(el, evt) {
 
-$(document).ready(function(){ 
-	$('input').on('input', function () {
-        this.value = this.value.replace(/[^0-9]/g, '');
-      });
-	});
+        var charCode = (evt.which) ? evt.which : event.keyCode;
+        var number = el.value.split('.');
+        // permitir el signo - (45) y . (46)
+        if (charCode != 45 && charCode != 46 && charCode > 31 && (charCode < 48 || charCode > 57)) {
+          return false;
+        }
+        //just one dot
+        if (number.length > 1 && charCode == 46) {
+          return false;
+        }
+        //get the character position
+        var caratPos = getSelectionStart(el);
+        // evitar mostrar signo - en una posicion diferente de la inicial
+        if (caratPos > 0 && charCode == 45) {
+          return false;
+        }
+        // no permtir mas de un - en el numero
+        if (charCode == 45 && el.value.charAt(0) == "-") {
+          return false;
+        }
+        var dotPos = el.value.indexOf(".");
+        if (caratPos > dotPos && dotPos > -1 && (number[1].length > 1)) {
+          return false;
+        }
+        else{
+          return true;
+        }
+    }
+      
+    function getSelectionStart(o) {
+        if (o.createTextRange) {
+          var r = document.selection.createRange().duplicate()
+          r.moveEnd('character', o.value.length)
+          if (r.text == '') return o.value.length
+          return o.value.lastIndexOf(r.text)
+        } else { return o.selectionStart }
+    }
--- suma-ajax.php
+++ suma-ajax.php
@@ -2,6 +2,18 @@
 if(($_POST['num1'] == '') || ($_POST['num2'] == '')){
     echo '<h4 class="text-danger">No se ha podido realizar la operación. Por favor, verifique que ha colocado valores en los campos y que estos sean válidos</h4>';
 }
+else if(($_POST['num1'] == '-') || ($_POST['num2'] == '-')){
+    echo '<h4 class="text-danger">No se ha podido realizar la operación. Por favor, verifique que ha colocado valores en los campos y que estos sean válidos</h4>';
+}
+else if(($_POST['num1'] == '.') || ($_POST['num2'] == '.')){
+    echo '<h4 class="text-danger">No se ha podido realizar la operación. Por favor, verifique que ha colocado valores en los campos y que estos sean válidos</h4>';
+}
+else if(($_POST['num1'] == '-.') || ($_POST['num2'] == '-.')){
+    echo '<h4 class="text-danger">No se ha podido realizar la operación. Por favor, verifique que ha colocado valores en los campos y que estos sean válidos</h4>';
+}
+else if(($_POST['num1'] == '.-') || ($_POST['num2'] == '.-')){
+    echo '<h4 class="text-danger">No se ha podido realizar la operación. Por favor, verifique que ha colocado valores en los campos y que estos sean válidos</h4>';
+}
 else{
     $total = $_POST['num1'] + $_POST['num2'];
     echo '<h4 class="text-success">El resultado es: '.$total.'</h4>';
